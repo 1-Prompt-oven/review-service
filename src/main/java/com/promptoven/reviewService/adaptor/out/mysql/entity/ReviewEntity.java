653d48b645ea142875d18582152785cdc826e368
flskhhdf <flskhhdf@naver.com>
2024-10-22 22:18:24 +0900

    Feat : 데이터 정의 - create를 위한 기본 Vo, Dto, Entity, Domain 정의

--- src/main/java/com/promptoven/reviewService/adaptor/out/mysql/entity/ReviewEntity.java
+++ src/main/java/com/promptoven/reviewService/adaptor/out/mysql/entity/ReviewEntity.java
@@ -1,5 +1,44 @@
 package com.promptoven.reviewService.adaptor.out.mysql.entity;
 
-public class ReviewEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor
+public class ReviewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Column(nullable = false)
+    private String productUuid;
+
+    @Column(nullable = false)
+    private String memberUuid;
+
+    @Column(nullable = false)
+    private int star;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private Boolean isDeleted;
+
+    @Builder
+    public ReviewEntity(Long reviewId, String productUuid, String memberUuid, int star, String contents, Boolean isDeleted) {
+        this.reviewId = reviewId;
+        this.productUuid = productUuid;
+        this.memberUuid = memberUuid;
+        this.star = star;
+        this.contents = contents;
+        this.isDeleted = isDeleted;
+    }
 }
--- src/main/java/com/promptoven/reviewService/domain/model/Review.java
+++ src/main/java/com/promptoven/reviewService/domain/model/Review.java
@@ -1,5 +1,33 @@
 package com.promptoven.reviewService.domain.model;
 
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class Review {
 
+    private Long id; // 비지니스 로직 또는 프론트에서 사용 할 수 있는 값
+    private String productUuid;
+    private String memberUuid;
+    private int star;
+    private String contents;
+    private Boolean isDeleted; // soft delete
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Review(Long id, String productUuid, String memberUuid, int star, String contents, Boolean isDeleted ,LocalDateTime createdAt,
+            LocalDateTime updatedAt) {
+        this.id = id;
+        this.productUuid = productUuid;
+        this.memberUuid = memberUuid;
+        this.star = star;
+        this.contents = contents;
+        this.isDeleted = isDeleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
